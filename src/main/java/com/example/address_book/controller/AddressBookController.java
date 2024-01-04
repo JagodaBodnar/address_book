eb29cda42f595299075dab00fbe45154ab291c5b
JagodaBodnar <jagodabodnar90@gmail.com>
2024-01-04 23:19:17 +0100

    Address Book - corrected methods street and name.

--- src/main/java/com/example/address_book/controller/AddressBookController.java
+++ src/main/java/com/example/address_book/controller/AddressBookController.java
@@ -1,10 +1,11 @@
-package controller;
-import exception.AddressesBookNotFoundException;
+package com.example.address_book.controller;
+
+import com.example.address_book.exception.AddressesBookNotFoundException;
+import com.example.address_book.model.AddressBook;
+import com.example.address_book.repository.AddressBookRepository;
 import jakarta.validation.Valid;
-import model.AddressBook;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import repository.AddressBookRepository;
 
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class AddressBookController {
 
     private final AddressBookRepository repository;
 
-    public AddressBookController(AddressBookRepository repository){
+    public AddressBookController(AddressBookRepository repository) {
         this.repository = repository;
     }
 
@@ -23,24 +24,37 @@ public class AddressBookController {
     public List<AddressBook> findAll() {
         return repository.findAll();
     }
+
     @GetMapping("/{id}")
     public AddressBook findById(@PathVariable String id) throws AddressesBookNotFoundException {
         return repository.findById(id);
     }
+
+    @GetMapping("/street/{street}")
+    public AddressBook findByStreetName(@PathVariable String street) throws AddressesBookNotFoundException {
+        return repository.findByStreetName(street);
+    }
+
+    @GetMapping("/name/{name}")
+    public AddressBook findByName(@PathVariable String name) throws AddressesBookNotFoundException {
+        return repository.findByName(name);
+    }
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public AddressBook create(@Valid @RequestBody AddressBook address){
-        return  repository.create(address);
+    public AddressBook create(@Valid @RequestBody AddressBook address) {
+        return repository.create(address);
     }
+
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    public void update(@Valid @RequestBody AddressBook address, @PathVariable String id){
+    public void update(@Valid @RequestBody AddressBook address, @PathVariable String id) {
         repository.update(address, id);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable String id){
+    public void delete(@PathVariable String id) {
         repository.delete(id);
     }
 }
--- src/main/java/com/example/address_book/repository/AddressBookRepository.java
+++ src/main/java/com/example/address_book/repository/AddressBookRepository.java
@@ -1,11 +1,11 @@
-package repository;
-import exception.AddressesBookNotFoundException;
-import model.AddressBook;
+package com.example.address_book.repository;
+import com.example.address_book.exception.AddressesBookNotFoundException;
+import com.example.address_book.model.AddressBook;
 import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
 @Repository
 public class AddressBookRepository {
     List<AddressBook> addresses = new ArrayList<>();
@@ -13,7 +13,8 @@ public class AddressBookRepository {
     public AddressBookRepository() {
         addresses.add(new AddressBook(UUID.randomUUID().toString(), "5th Avenue", "New York", "112233", "Jane", "Doe"));
     }
-    public List<AddressBook> findAll(){
+
+    public List<AddressBook> findAll() {
         return addresses;
     }
 
@@ -22,25 +23,25 @@ public class AddressBookRepository {
     }
 
     public AddressBook findByStreetName(String street) throws AddressesBookNotFoundException {
-        return addresses.stream().filter(address -> address.getId().equals(street)).findAny().orElseThrow(AddressesBookNotFoundException::new);
+        return addresses.stream().filter(address -> address.getStreet().equals(street)).findFirst().orElseThrow(AddressesBookNotFoundException::new);
     }
+
     public AddressBook findByName(String name) throws AddressesBookNotFoundException {
-        return addresses.stream().filter(address -> address.getId().equals(name)).findAny().orElseThrow(AddressesBookNotFoundException::new);
+        return addresses.stream().filter(address -> address.getName().equals(name)).findFirst().orElseThrow(AddressesBookNotFoundException::new);
     }
 
-
-    public AddressBook create(AddressBook address){
+    public AddressBook create(AddressBook address) {
         addresses.add(address);
         return address;
     }
 
-    public void update(AddressBook address, String id){
-        AddressBook existing = addresses.stream().filter(a-> a.getId().equals(id)).findFirst().orElseThrow(()-> new IllegalArgumentException("Stream not found"));
+    public void update(AddressBook address, String id) {
+        AddressBook existing = addresses.stream().filter(a -> a.getId().equals(id)).findFirst().orElseThrow(() -> new IllegalArgumentException("Stream not found"));
         int i = addresses.indexOf(existing);
         addresses.set(i, address);
     }
-    public void delete(String id){
+
+    public void delete(String id) {
         addresses.removeIf(addresses -> addresses.getId().equals(id));
     }
-
 }
